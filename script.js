b69c3cac0cbcf533b38044f3f407f94b8173022d
Cesar Constanzo <tron31.cc@gmail.com>
2020-09-17 20:19:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function menutoggle() {
+  var MenuItems  = document.getElementById("MenuItems");
+  if (MenuItems.style.display === "none") {
+    MenuItems.style.display = "block";
+  } else {
+    MenuItems.style.display = "none";
+  }
+}
+var ProductImg = document.getElementById("ProductImg");
+  var SmallImg = document.getElementsByClassName("small-img");
+  SmallImg[0].onclick = function(){
+    ProductImg.src = SmallImg[0].src;
+  }
+  SmallImg[1].onclick = function(){
+    ProductImg.src = SmallImg[1].src;
+  }
+  SmallImg[2].onclick = function(){
+    ProductImg.src = SmallImg[2].src;
+  }
+  SmallImg[3].onclick = function(){
+    ProductImg.src = SmallImg[3].src;
+  }
